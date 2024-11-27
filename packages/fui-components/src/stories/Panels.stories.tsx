84efcf8af17668f7b85111dd4b1cf8a12ebb924b
Brandon Abbott <pbabbott@gmail.com>
2024-11-26 22:14:28 -0600

    fix: tidy up panel showcase story

--- packages/fui-components/src/stories/Panels.stories.tsx
+++ packages/fui-components/src/stories/Panels.stories.tsx
@@ -1,8 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
 
 import { Panel, Typography, Button } from "../index";
-import { PanelColor } from "../components/Panel/types";
+import { PanelColor, PanelVariant } from "../components/Panel/types";
 import { ButtonColor } from "../components/Button/Button";
 
 const meta = {
@@ -11,9 +10,16 @@ const meta = {
   parameters: {
     layout: "centered",
   },
-  args: {
-    onClick: fn(),
+  argTypes: {
+    variant: {
+      control: "select",
+      options: ["transparent", "outlined", "dots"],
+      description: "The variant of the panel",
+    },
   },
+  args: {
+    variant: 'transparent'
+  }
 } satisfies Meta<typeof Panel>;
 
 export default meta;
@@ -22,15 +28,17 @@ type Story = StoryObj<typeof meta>;
 const SamplePanel = ({
   color,
   buttonColor,
+  variant = 'transparent'
 }: {
   color: PanelColor;
   buttonColor: ButtonColor;
+  variant?: PanelVariant;
 }) => (
-  <Panel className="max-w-72 px-10 py-5" variant="transparent" color={color}>
+  <Panel className="max-w-80 px-8 py-4" variant={variant} color={color}>
     <Typography variant="h4" component="h4">
       I am a header
     </Typography>
-    <Typography className="mb-8" variant="body2" component="p">
+    <Typography className="mb-12" variant="body2" component="p">
       The quick brown fox jumped over the lazy dog.
     </Typography>
 
@@ -40,10 +48,13 @@ const SamplePanel = ({
   </Panel>
 );
 
-export const TransparentShowcase: Story = {
-  render: () => (
-    <div className="flex gap-4">
-      <SamplePanel color="default" buttonColor="secondary" />
+export const SimpleShowcase: Story = {
+  render: (args) => (
+    <div className="flex gap-8">
+      <SamplePanel color="default" buttonColor="secondary" variant={args.variant} />
+      <SamplePanel color="white" buttonColor="primary" variant={args.variant} />
+      <SamplePanel color="primary" buttonColor="primary" variant={args.variant} />
+      <SamplePanel color="secondary" buttonColor="secondary" variant={args.variant} />
     </div>
   ),
 };
