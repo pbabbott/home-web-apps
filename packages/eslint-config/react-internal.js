acf06377e0b940b1001526da59f2fe489c00efcb
Brandon Abbott <pbabbott@gmail.com>
2024-12-04 19:36:30 -0600

    fix: add react refresh to eslint package

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,5 +1,6 @@
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginReact from "eslint-plugin-react";
+import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from "globals";
 import baseConfigs from "./base.js";
 
@@ -22,11 +23,16 @@ export default [
   {
     plugins: {
       "react-hooks": pluginReactHooks,
+      "react-refresh": reactRefresh,
     },
     settings: { react: { version: "detect" } },
     rules: {
       ...pluginReactHooks.configs.recommended.rules,
       // React scope no longer necessary with new JSX transform.
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
       "react/react-in-jsx-scope": "off",
     },
   },
