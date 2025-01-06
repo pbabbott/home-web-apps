679c30e0ea45eb15a182d5cd2077819326352f48
Brandon Abbott <pbabbott@gmail.com>
2025-01-06 17:42:34 +0000

    Address build issues with ts libraries and servers.

--- apps/gluetun-sync/src/index.ts
+++ apps/gluetun-sync/src/index.ts
@@ -1,18 +1,17 @@
 import 'reflect-metadata'
 import { initConfig, validateConfig } from './config'
-import { startServer } from "./server";
-import { cronJob, startCron } from './cron';
+import { startServer } from "./server"
+import { cronJob, startCron } from './cron'
 
 const start = async () => {
   await initConfig()
-  validateConfig();
+  validateConfig()
 
-  startCron();
-  startServer();
+  startCron()
+  startServer()
 
   // Run cron once at the start after 10 seconds
   setTimeout(cronJob, 1000*10)
-    
 };
 
 start();
