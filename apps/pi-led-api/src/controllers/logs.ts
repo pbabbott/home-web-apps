f5c95f6f1054108937aad5f37d3eab8c0e4cf01c
Brandon Abbott <pbabbott@gmail.com>
2025-03-03 02:47:13 +0000

    implement basic logging endpoint for pi-led-api

--- apps/pi-led-api/src/controllers/logs.ts
+++ apps/pi-led-api/src/controllers/logs.ts
@@ -1,3 +1,20 @@
 import { Request, Response } from 'express';
+import { readFileSync } from 'fs';
+import path from 'path';
 
-export const getLogs = (req: Request, res: Response) => {};
+export const getLogs = (req: Request, res: Response) => {
+  try {
+    const logFilePath = path.resolve('/opt/pironman/log');
+    // TODO: add validation
+    const { lines = 10 } = req.query;
+
+    const logContent = readFileSync(logFilePath, 'utf-8');
+    const logLines = logContent.split('\n');
+    const recentLogLines = logLines.slice(-lines as number);
+
+    res.send({ logEntries: recentLogLines });
+  } catch (error) {
+    console.error('Error reading log file:', error);
+    res.status(500).send({ error: 'Failed to read log file' });
+  }
+};
