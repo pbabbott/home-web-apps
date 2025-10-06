12cde2ff1564b7bd139308c7f1d08daf5c5bf5cd
Brandon Abbott <pbabbott@gmail.com>
2025-10-06 01:19:53 +0000

    Upgrade to express v5

--- packages/express/src/routes/health-route.ts
+++ packages/express/src/routes/health-route.ts
@@ -1,6 +1,7 @@
-import { Router } from 'express';
-export const configureHealthRoute = (router: Router) => {
-  router.get('/healthz', (_, res) => {
+import { Express } from 'express';
+
+export const configureHealthRoute = (app: Express) => {
+  app.get('/healthz', (_, res) => {
     return res.json({ ok: true });
   });
 };
