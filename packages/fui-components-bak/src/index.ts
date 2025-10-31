ed51c954a3ea0690bcab3e1e6ee709c528be6f2e
Brandon Abbott <pbabbott@gmail.com>
2025-10-31 19:26:42 +0000

    remove fui-components bak

--- packages/fui-components-bak/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { Button, type ButtonProps } from './components/Button/Button';
-export { Panel, type PanelProps } from './components/Panel/Panel';
-export {
-  Typography,
-  type TypographyProps,
-} from './components/Typography/Typography';
